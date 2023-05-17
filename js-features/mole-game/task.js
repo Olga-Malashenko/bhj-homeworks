f6c2ee49a4a8fda8020f190a5ed2b524a27cfd46
Olga Malashenko <olga_birjukova@mail.ru>
2023-05-17 14:33:01 +0300

    Задание реализовано

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,26 @@
+const holes = document.querySelectorAll('.hole');
+const deadCounter = document.querySelector('#dead');
+const lostCounter = document.querySelector('#lost');
+let dead = 0;
+let lost = 0;
+
+for (let hole of holes) {
+    hole.addEventListener('click',  handler);
+    
+}
+
+function handler(e) {
+    if (e.target.classList.contains('hole_has-mole')) {
+        dead += 1;
+        deadCounter.textContent = dead;
+    } else {
+        lost += 1;
+        lostCounter.textContent = lost;
+    }
+    if (lost === 5 || dead === 10) {
+        dead =0;
+        lost =0;
+        setTimeout(() => alert('GAME OVER'), 200) ;
+    }
+}
+
