3a6174cf648915a919571d6560e6879520ce447a
Olga Malashenko <olga_birjukova@mail.ru>
2023-06-18 00:22:58 +0300

    Попытка реализовать первую задачу

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,23 @@
+const phrases = document.querySelectorAll('.has-tooltip');
+
+
+for (let phrase of phrases) {
+    phrase.addEventListener('click', handler);
+}
+
+const tooltip = document.createElement('div');
+tooltip.classList.add('tooltip');
+tooltip.style = 'left: 0; top: 0';
+
+function handler(e) {
+    e.preventDefault();
+
+    let currentPhrase = e.target;
+
+    tooltip.classList.add('tooltip_active');
+    tooltip.textContent = currentPhrase.title;
+    console.log(tooltip);
+
+    currentPhrase.insertAdjacentHTML('afterEnd', tooltip);
+
+}
