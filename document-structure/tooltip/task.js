5c35c4b97fd522cb098a763c56396de29c8150d0
Olga Malashenko <olga_birjukova@mail.ru>
2023-06-20 02:10:05 +0300

    Попытка разобраться с первой задачей, неудачная

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -5,19 +5,27 @@ for (let phrase of phrases) {
     phrase.addEventListener('click', handler);
 }
 
-const tooltip = document.createElement('div');
-tooltip.classList.add('tooltip');
-tooltip.style = 'left: 0; top: 0';
+
 
 function handler(e) {
     e.preventDefault();
 
     let currentPhrase = e.target;
+    //console.log(currentPhrase);
 
-    tooltip.classList.add('tooltip_active');
-    tooltip.textContent = currentPhrase.title;
-    //console.log(tooltip);
+    let tooltip = document.createElement('div');
+    tooltip.classList.add('tooltip');
+    tooltip.style = 'left: 0; top: 0';
 
-    currentPhrase.insertAdjacentHTML('afterend', tooltip);
+    if (! tooltip.classList.contains('tooltip_active')) {
+        tooltip.classList.add('tooltip_active');
+        tooltip.textContent = currentPhrase.title;
+        //console.log(tooltip);
+
+        currentPhrase.insertAdjacentHTML('afterEnd', tooltip);
+    } else {
+        tooltip.remove();
+    }
+    
 
 }
