5ce2e8954aaeef19f71961f842cc5a4f596592ff
Olga Malashenko <olga_birjukova@mail.ru>
2023-06-21 00:05:21 +0300

    Задача реализована полностью (в рамках обязательного задания)

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -4,30 +4,6 @@ const addButtons = document.querySelectorAll('.product__add');
 const cart = document.querySelector('.cart__products');
 let count = 1;
 
-for (let btn of addButtons) {
-    btn.addEventListener('click', (e) => {
-        let product = btn.closest('.product');
-        let productCart = document.createElement('div');
-        productCart.classList.add('cart__product');
-        productCart.dataset.id = product.dataset.id;
-
-        let image = btn.closest('.product__controls').previousElementSibling;
-        let productImage = document.createElement('img');
-        productImage.classList.add('cart__product-image');
-        productImage.src = image.src;
-
-        let productQuantity = document.createElement('div');
-        productQuantity.classList.add('cart__product-count');
-        productQuantity.textContent = count;
-        count = 1;
-        console.log( count, productQuantity, productImage) ;
-
-        cart.appendChild(productCart);
-        productCart.appendChild(productImage);
-        productCart.appendChild(productQuantity);
-    })
-}
-
 for (let btn of quantityControlDec) {
     btn.addEventListener('click', (e) => {
         let quantity = btn.nextElementSibling;
@@ -45,4 +21,45 @@ for (let btn of quantityControlInc) {
         count = Number(quantity.textContent);
     })
 }
+
+
+
+for (let btn of addButtons) {
+    btn.addEventListener('click', (e) => {
+        let currentProduct = e.target.previousElementSibling;
+        let currentCount = currentProduct.querySelector('.product__quantity-value').textContent;
+
+        let product = btn.closest('.product');
+        let productCart = document.createElement('div');
+        productCart.classList.add('cart__product');
+        productCart.dataset.id = product.dataset.id;
+
+        let image = btn.closest('.product__controls').previousElementSibling;
+        let productImage = document.createElement('img');
+        productImage.classList.add('cart__product-image');
+        productImage.src = image.src;
+
+        let productQuantity = document.createElement('div');
+        productQuantity.classList.add('cart__product-count');
+        productQuantity.textContent = count;
+        
+       let addedProducts = Array.from(document.querySelectorAll('.cart__product'));
+
+
+        if (addedProducts && addedProducts.some(item => item.dataset.id === product.dataset.id)) {
+            productCart = addedProducts.find(item => item.dataset.id === product.dataset.id);
+            let newCount = Number(productCart.lastElementChild.textContent) + Number(currentCount);
+            productCart.lastElementChild.textContent = newCount;
+        }
+        else {
+            cart.appendChild(productCart);
+            productCart.appendChild(productImage);
+            productCart.appendChild(productQuantity);
+        }
+        
+        count = 1;
+    })
+}
+
+
 
