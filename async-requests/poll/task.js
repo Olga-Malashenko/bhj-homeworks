b94e19462b75df049278c9829b8c936a029d0212
Olga Malashenko <olga_birjukova@mail.ru>
2023-10-01 23:35:30 +0300

    Styles are changed

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -8,17 +8,16 @@ const xhr = new XMLHttpRequest();
 xhr.addEventListener('readystatechange', ()=> {
     if(xhr.readyState === xhr.DONE) {
         const question = JSON.parse(xhr.responseText);
-        //console.log(question);
 
         pollTitle.textContent = question.data.title;
         answers = question.data.answers;
         numberVote = question.id;
-        console.log(numberVote);
+        pollAnswers.classList.remove('poll__answers');
+        pollAnswers.classList.add('poll__answers_active');
 
         answers.forEach(element => {
             let pollAnswer = document.createElement('button');
-            pollAnswers.classList.add('poll__answers');
-            pollAnswer.classList.remove('poll__answers_active');
+            pollAnswer.classList.add('poll__answer');
             pollAnswer.textContent = element;
             pollAnswers.appendChild(pollAnswer);
         });  
@@ -30,52 +29,33 @@ xhr.send();
 
 pollAnswers.addEventListener('click', (e)=> {
     alert('Спасибо, ваш голос засчитан!');
-    //console.log(e.target);
-    //console.log(answers);
     numberAnswer = answers.indexOf(e.target.textContent);
-    console.log(`Индекс : ${numberAnswer}`);
 
     const xhr = new XMLHttpRequest();
     xhr.addEventListener('readystatechange', ()=> {
         if (xhr.readyState === xhr.DONE) {
-            pollAnswers.classList.add('poll__answers_active');
-            pollAnswers.classList.remove('poll__answers');
+            pollAnswers.classList.remove('poll__answers_active');
+            pollAnswers.classList.add('poll__answers');
             let response = xhr.responseText;
             let resp = JSON.parse(response);
             
-            //console.log(resp.stat);
             let sum = 0;
             
             for (let element of resp.stat) {
-                //console.log(`Количество голосов: ${element.votes}`);
                 sum += element.votes;
-                console.log(sum);
             }
+
             for (let element of resp.stat) {
                 let procent = element.votes / sum * 100;
                 let stat = document.createElement('div');
                 stat.textContent = `${element.answer} : ${procent.toFixed(2)}%`;
-                //console.log(element.answer);
-                
-                //console.log(procent.toFixed(2));
                 pollTitle.appendChild(stat);
             }
         }
-        
-
     });
+
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll');
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.send(`vote=${numberVote}&answer=${numberAnswer}`);
-    
 });
-
-
-
-
-
-
-
-
-
 
