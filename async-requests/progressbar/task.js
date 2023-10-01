93b62c611409b534b962f1043ff397a263191563
Olga Malashenko <olga_birjukova@mail.ru>
2023-10-02 00:03:09 +0300

    Event is changed

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -3,27 +3,14 @@ const progress = document.querySelector('#progress');
 
 form.addEventListener('submit', ()=> {
     e.preventDfault();
+    const formData = new FormData(form);
     const xhr = new XMLHttpRequest();
+    
+    xhr.upload.addEventListener('progress', (e)=> {
+        progress.value = e.loaded;
+    });
+    
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
     xhr.send(formData);
-    xhr.addEventListener('readystatechange', ()=> {
-        if(xhr.readyState === 1) {
-            progress.value = 0.2;
-        }
-        if(xhr.readyState === 2) {
-            progress.value = 0.5;
-        }
-        if(xhr.readyState === 3) {
-            progress.value = 0.75;
-        }
-        if(xhr.readyState === xhr.DONE) {
-            progress.value = 1;
-        }
-    });
-    const formData = new FormData(form);
+    
 });
-
-
-
-
-
