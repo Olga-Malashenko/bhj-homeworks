e3ae81a338634f81ad0cb982771fee5408ea36fe
Olga Malashenko <olga_birjukova@mail.ru>
2023-10-02 16:44:16 +0300

    Preloader Fixed mistake of attachment

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,81 +1,53 @@
 const loader = document.querySelector('#loader');
 const items = document.querySelector('#items');
 
-let storedData;
+let storedData = localStorage.getItem('key');
 const xhr = new XMLHttpRequest();
 
-if (storedData) {
-    console.log('есть');
-    // вывести из локалСтор данные
-    // ...
+xhr.addEventListener('readystatechange', () => {
+    if (storedData) {
+        console.log('есть');
+        // вывести из локалСтор данные
+        // ...
+    } else {
+        console.log('пусто');
+    }
 
-    // получить новые и перезаписать textContent и localStorage
+    if (xhr.readyState === xhr.DONE) {
+        loader.classList.remove('loader_active');
 
-    xhr.addEventListener('readystatechange', ()=> {
-        if (xhr.readyState === xhr.DONE) {
-            let response = JSON.parse(xhr.responseText).response.Valute;
+        let response = JSON.parse(xhr.responseText).response.Valute;
 
-            for (let key in response) {
-                
-                code.textContent = response[key].CharCode;
-                value.textContent = response[key].Value;
-    
-                let object = {
-                    code: response[key].CharCode,
-                    value: response[key].Value
-                }
-    
-                localStorage.setItem(key, JSON.stringify(object));
-                storedData = localStorage.getItem(key);
-    
+        for (let key in response) {
+            let item = document.createElement('div');
+            item.classList.add('item');
+            items.appendChild(item);
+
+            let code = document.createElement('div');
+            code.classList.add('item__code');
+            code.textContent = response[key].CharCode;
+
+            let value = document.createElement('div');
+            value.classList.add('item__value');
+            value.textContent = response[key].Value;
+
+            let currency = document.createElement('div');
+            currency.classList.add('item__currency');
+            currency.textContent = ' руб.';
+
+            let object = {
+                code: response[key].CharCode,
+                value: response[key].Value
             }
-        }
-    });
 
-} else {
-    console.log('пусто');
-    xhr.addEventListener('readystatechange', ()=> {
-    
-        if (xhr.readyState === xhr.DONE) {
-            loader.classList.remove('loader_active');
-    
-            let response = JSON.parse(xhr.responseText).response.Valute;
-    
-            for (let key in response) {
-                let item = document.createElement('div');
-                item.classList.add('item');
-                items.appendChild(item);
-    
-                let code = document.createElement('div');
-                code.classList.add('item__code');
-                code.textContent = response[key].CharCode;
-    
-                let value = document.createElement('div');
-                value.classList.add('item__value');
-                value.textContent = response[key].Value;
-    
-                let currency = document.createElement('div');
-                currency.classList.add('item__currency');
-                currency.textContent = ' руб.';
-    
-                let object = {
-                    code: response[key].CharCode,
-                    value: response[key].Value
-                }
-    
-                localStorage.setItem(key, JSON.stringify(object));
-                storedData = localStorage.getItem(key);
-    
-                item.appendChild(code);
-                item.appendChild(value);
-                item.appendChild(currency);
-            }
-            console.log(`after: ${storedData}`);
-        }
-    });
-}
+            localStorage.setItem('key', JSON.stringify(object));
 
-//const xhr = new XMLHttpRequest();
+            item.appendChild(code);
+            item.appendChild(value);
+            item.appendChild(currency);
+        }
+    }
+});
 
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
 xhr.send();
