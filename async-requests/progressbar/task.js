5a63b43a64b4450adbd29b91fdce638e93a0fb94
Olga Malashenko <olga_birjukova@mail.ru>
2023-10-04 18:33:30 +0300

    Progressbar is done

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,13 +1,13 @@
 const form = document.querySelector('#form');
 const progress = document.querySelector('#progress');
 
-form.addEventListener('submit', ()=> {
-    e.preventDfault();
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
     const formData = new FormData(form);
     const xhr = new XMLHttpRequest();
     
     xhr.upload.addEventListener('progress', (e)=> {
-        progress.value = e.loaded;
+        progress.value = e.loaded / e.total;
     });
     
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
