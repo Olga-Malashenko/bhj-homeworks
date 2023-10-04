5a28e5139c4498e42ca36d6612a754749bcf37fc
Olga Malashenko <olga_birjukova@mail.ru>
2023-10-04 21:05:40 +0300

    Preloader is done completely

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,30 +1,13 @@
 const loader = document.querySelector('#loader');
 const items = document.querySelector('#items');
 
-//let storedData = JSON.parse(localStorage.getItem('data'));
-//let allStoredValutes;
-//console.log(`verh: ${allStoredValutes}`);
-//storedData = [...allStoredValutes];
-//console.log(storedData);
-let fromStorage = JSON.parse(localStorage.getItem('data'));;
-//let storedData = fromStorage;
-
-//let storedInfo = localStorage.getItem('storedAll');
-
-//console.log(storedInfo);
+let fromStorage = JSON.parse(localStorage.getItem('data'));
+//let fromStorage;
 
 document.addEventListener("DOMContentLoaded", () => {
     if (fromStorage) {
         console.log('есть');
-        //console.log();
-        console.log(fromStorage[3]);
-        //for (let element of storedData) {
-        //console.log(storedData[i].code);
-        //};
-
-        //console.log(storedData);
-        // вывести из локалСтор данные
-        // ...
+        createValuteList(fromStorage);
     } else {
         console.log('запрос');
         const xhr = new XMLHttpRequest();
@@ -37,6 +20,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
                 let allStoredValutes = [];
 
+                createValuteList(response);
+
                 for (let key in response) {
                     let item = document.createElement('div');
                     item.classList.add('item');
@@ -45,41 +30,28 @@ document.addEventListener("DOMContentLoaded", () => {
                     let code = document.createElement('div');
                     code.classList.add('item__code');
                     code.textContent = response[key].CharCode;
+                    item.appendChild(code);
 
                     let value = document.createElement('div');
                     value.classList.add('item__value');
+
                     value.textContent = response[key].Value;
+                    item.appendChild(value);
 
                     let currency = document.createElement('div');
                     currency.classList.add('item__currency');
                     currency.textContent = ' руб.';
-
-                    item.appendChild(code);
-                    item.appendChild(value);
                     item.appendChild(currency);
 
                     let object = {
-                        code: response[key].CharCode,
-                        value: response[key].Value
+                        CharCode: response[key].CharCode,
+                        Value: response[key].Value
                     }
-                    //console.log(object);
-                    //console.log(JSON.stringify(object));
-
-                    //localStorage.setItem(key, JSON.stringify(object));
-
-                    //let storedValute = JSON.parse(localStorage.getItem(key));
-                    //console.log(storedValute);
 
                     allStoredValutes.push(object);
                 }
-                console.log(`allSumma: ${allStoredValutes}`);
-                //let newObjectForMemory = JSON.stringify(allStoredValutes);
+
                 localStorage.setItem('data', JSON.stringify(allStoredValutes));
-                //let fromStorage = localStorage.getItem('data');
-                
-                console.log(`Memory: ${fromStorage}`);
-                console.log(fromStorage[3]); //it's object
-                console.log(Array.isArray(fromStorage)); //true
             }
         });
 
@@ -87,5 +59,29 @@ document.addEventListener("DOMContentLoaded", () => {
         xhr.send();
     }
 })
+
+function createValuteList(list) {
+    for (let key in list) {
+        let item = document.createElement('div');
+        item.classList.add('item');
+        items.appendChild(item);
+
+        let code = document.createElement('div');
+        code.classList.add('item__code');
+        code.textContent = list[key].CharCode;
+        item.appendChild(code);
+
+        let value = document.createElement('div');
+        value.classList.add('item__value');
+
+        value.textContent = list[key].Value;
+        item.appendChild(value);
+
+        let currency = document.createElement('div');
+        currency.classList.add('item__currency');
+        currency.textContent = ' руб.';
+        item.appendChild(currency);
+    }
+}
 
 
