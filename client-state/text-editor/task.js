27397ba6f514d75e0a33d650b18659665ea35d19
Olga Malashenko <olga_birjukova@mail.ru>
2023-10-04 23:25:14 +0300

    text-editor is done

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,10 +1,25 @@
 const editor = document.querySelector('#editor');
+const card = document.querySelector('.card');
+let button = document.querySelector('.button__clear');
+
+button.addEventListener('click', () => {
+    editor.value = '';
+    button.classList.remove('button__clear_active');
+});
+
 let storedContent = localStorage.getItem('content');
 
 if (storedContent) {
     editor.value = storedContent;
+    button.classList.add('button__clear_active');
 }
 
-editor.addEventListener('input', ()=> {
+editor.addEventListener('input', () => {
+    if (editor.value && editor.value !=='') {
+        button.classList.add('button__clear_active');
+    } else {
+        button.classList.remove('button__clear_active');
+    }
     localStorage.setItem('content', editor.value);
 });
+
